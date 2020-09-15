4bebc17c6cbf401098abeef0a3c888cda1002926
inform <inform@CLASS-04.kl9.uspu.ru>
2020-09-15 17:22:23 +0500

    Длину окружности и площадь круга считает

--- hi_world/main.cc
+++ hi_world/main.cc
@@ -6,9 +6,35 @@
  */
 
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+
+constexpr double Pi = acos(-1.);
+
 int main()
 {
 	std::cout <<"Привет, мир!" << std::endl;
+	std:: cout <<"Длина окружности и "
+			"площадь круга"<< std::endl;
+
+
+	std::cout << "Введите радиус окружности " ;
+	double radius;
+	std::cin >> radius;
+
+	double
+	length = 2* Pi* radius,
+	area = Pi* radius * radius;
+
+	std::cout <<
+			std::fixed <<
+			"Длина окружности = " << std::setw(9) << std::setprecision(3) <<
+			length << std::endl <<
+			"Площадь круга    = " <<
+			std::setw(9) <<
+			std::setprecision(3) <<
+			area << std::endl;
+
 	return 0;
 }
 
